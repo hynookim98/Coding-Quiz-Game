949f149704e410203c785c2a714a588890fe8520
hynookim98 <hynookim98@gmail.com>
2022-11-16 15:03:33 -0800

    added hide display to css, started on highscore.html, got choices to show up on click

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 // initializing variables
 var startButton = document.getElementById('start');
-var questionsEl = document.getElementById('questions');
+var questionsEl = document.getElementById('problems');
 var timer = document.getElementById('time');
+var question = document.getElementById('question');
+var choices = document.getElementById('choices');
+
 
 // giving user around 10 seconds per question
 var time = questionList.length * 10;
+var questionIndex = 0;
 
+// function runs at first when start button is pressed
 function startQuizGame() {
 
     var mainPage = document.getElementById('main-page');
@@ -30,8 +35,19 @@ function countdown() {
 }
 
 function chooseQuestion() {
+    var problem = questionList[questionIndex];
 
+    question.textContent = problem.title;
+
+    for (var i=0; i < problem.choices.length; i++) {
+        var diffChoices = problem.choices[i];
+        var choiceBtn = document.createElement('button');
+
+        choiceBtn.textContent = diffChoices;
+        choices.append(choiceBtn); //append child???
+    }
 }
 
 // event listeners
+// when user presses start button it will run function startQuizGame
 startButton.onclick = startQuizGame;
